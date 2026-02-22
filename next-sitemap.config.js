bd251490c8eb291f2d80bfad3d78a293b87e6321
just-zyilzz <enzilaja@gmail.com>
2026-02-22 21:58:14 +0700

    chore: remove conflicting next-sitemap package to use native sitemap.ts

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://inversave.space";
-
-module.exports = {
-  siteUrl,
-  generateRobotsTxt: false,
-  sitemapSize: 7000,
-  exclude: ["/api/*"],
-};
